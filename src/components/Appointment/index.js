ab2761359891f3f023009f5512e6e8cee0a732d3
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-13 17:23:04 -0600

    create components, stories, and refcator components for Show, Confirm, Status, Error

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Empty from "./Empty";
+import Show from "./Show";
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
 
 export default function Appointment(props) {
   return(
