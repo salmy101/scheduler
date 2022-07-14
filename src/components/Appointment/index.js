0bdba5f27a5ea8cf107b133514d74e68e8684d32
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-14 15:35:07 -0600

    add Appointment Empty and Appointment booked views in stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Empty from "./Empty";
@@ -10,6 +10,12 @@ import Form from "./Form";
 
 export default function Appointment(props) {
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? 
+      <Show student={props.interview.student} 
+      interviewer={props.interview.interviewer}/>: <Empty/>}
+    </article>
+    
   );
 }
