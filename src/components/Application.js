24781541b66d776e7caee90e195fee1bf2f2ab90
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-16 15:07:58 -0600

    retrieve interviewer data, use helper to refactor into correct object, display interviewers names

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,93 +3,48 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
 import axios from "axios";
-import { getAppointmentsForDay } from "../helpers/Selectors.js";
-
-
-
-// const appointments = {
-//   1: {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   2: {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   3: {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   4: {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       },
-//     },
-//   },
-//   5: {
-//     id: 5,
-//     time: "4pm",
-//   },
-// };
-
-
+import { getAppointmentsForDay, getInterview } from "../helpers/Selectors.js";
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-  console.log(state)
+  console.log(state.interviewers)
 
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState({ ...state, days });
   const setAppointments = appointments => setState({ ...state, appointments });
-  const setMany = many => setState({ ...state, ...many });
 
+  const setMany = many => setState({ ...state, ...many });
 
   useEffect(() => {
     const daysURL = `http://localhost:8001/api/days`;
     const apptURL = `http://localhost:8001/api/appointments`;
+    const intrURL = `http://localhost:8001/api/interviewers`;
       Promise.all([
         axios.get(daysURL),
-        axios.get(apptURL)
+        axios.get(apptURL),
+        axios.get(intrURL)
       ]).then((all) => {
-        // setDays([...response.data])
-        // setState(prev => ({ ...prev, days }));
-        // console.log(all)
-
-        setMany({days: all[0].data, appointments: all[1].data})
-
-        // console.log("DAYS:", all[0].data);
-        // console.log("APPOINTMENTS:", all[1].data);
+        setMany({days: all[0].data, appointments: all[1].data, interviewers: all[2].data})
       })
     }, []);
   
-
-
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const schedule = dailyAppointments.map(appointment =>{
-    return (
-      <Appointment 
-        key={appointment.id} 
-         {...appointment} 
-      />
+    const interview = getInterview(state, appointment.interview);
+      return (
+        <Appointment
+          key={appointment.id}
+          id={appointment.id}
+          time={appointment.time}
+          interview={interview}
+          
+        />
     );
   })
 
@@ -108,7 +63,6 @@ export default function Application(props) {
             value={state.day} 
             onChange={setDay}
             // setDay={.....}
-
             />
         </nav>
         <img
--- src/helpers/Selectors.js
+++ src/helpers/Selectors.js
@@ -1,6 +1,6 @@
 export function getAppointmentsForDay(state, day) {
   const filteredDays = [];
-  for (const obj of state.days) { 
+  for (const obj of state.days) {
     if (obj.name === day) {
       for (const appt of obj.appointments) {
         if (state.appointments[appt]) {
@@ -11,3 +11,14 @@ export function getAppointmentsForDay(state, day) {
   }
   return filteredDays;
 }
+
+export function getInterview(state, interview) { 
+  const obj = {};
+  if(interview) { //if given an interview
+    obj["student"] = interview.student // the new obj will have name of student
+    obj["interviewer"] = state.interviewers[interview.interviewer] // and the interviwer will have object
+  } else {
+    return null
+  }
+   return obj;
+} 
