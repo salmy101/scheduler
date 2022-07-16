84ba5fc3f4182e0f70075f1f3cf2f0076b0af885
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-15 21:27:21 -0600

    manage state in app.js, combine states and refactor using spread operator

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,17 +45,25 @@ const appointments = {
   },
 };
 
-export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  console.log(day);
 
-  const [days, setDays] = useState([]);
+
+export default function Application(props) {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days =>setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const daysURL = `http://localhost:8001/api/days`;
     axios.get(daysURL).then(response => {
       setDays([...response.data])
-      console.log(response.data);
+      // console.log(response.data);
     });
   }, []);
   
@@ -78,7 +86,11 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList 
+            days={state.days}  
+            value={state.day} 
+            onChange={setDay}
+            />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
