f04aa55a15f61ce0069a32c1b0bbb830839c5a42
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-16 19:07:56 -0600

    build the tranistion and back hook and testing with jest

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); // This line is new!
+
+  function transition(newMode, replace = false) { //set the new view as the mode
+    setMode(newMode);
+
+    if (replace) {
+      history.pop();
+      setHistory((prev) => [...prev, newMode]);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
+  }
+
+  function back() { //to go back, if history array more then 1
+    if (history.length >  1) { //remove the last one
+      history.pop(); 
+      setMode(history[history.length - 1]); //set the new mode to whatever the histry was  minus one
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back,
+  };
+}
