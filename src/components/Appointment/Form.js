f81524bfb19aac97a037c8cb97fdc306cef0e22e
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-20 13:43:23 -0600

    write jest test for error handling when name i sblack and if interviewer is null

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,8 @@ import { useState } from 'react';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
 
   const reset = function(){
     setStudent("")
@@ -16,35 +18,48 @@ export default function Form(props) {
   const cancel = function(){
     reset()
     props.onCancel()
-
   }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }   if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  } 
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form onSubmit={(event) => event.preventDefault()}autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="student name"
+            name="student-name"
             type="text"
             value={student}
             onChange={(event) => {setStudent(event.target.value)}}
             placeholder="Enter Student Name"
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
         /* your code goes here */
         interviewers={props.interviewers}
         value={interviewer}
         onChange={setInterviewer}
-      
-        />
+       />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={() => props.onSave(student, interviewer)} confirm>
+          <Button onClick={() => validate(student, interviewer)} confirm>
             Save 
           </Button>
         </section>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,14 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewList.scss"
+import PropTypes from 'prop-types';
+
 
 export default function InterviewerList(props) {
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
+  
   const interviewers = props.interviewers.map((interviewer) => {
     return(
       <InterviewerListItem
